308b833f5eaf180e5c6c30a74daf2db4c3d6ea10
Olivier Roussel <olivier.roussel@inria.fr>
2023-11-29 16:41:45 +0100

    Add conda recipes.

---
+++ .github/workflows/conda/recipes/pinocchio/build_pinocchio.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+rm -rf build
+
+mkdir build
+cd build
+
+export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+echo $BUILD_NUMPY_INCLUDE_DIRS
+echo $TARGET_NUMPY_INCLUDE_DIRS
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+fi
+
+cmake ${CMAKE_ARGS} .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBUILD_WITH_URDF_SUPPORT=ON \
+      -DBUILD_WITH_COLLISION_SUPPORT=ON \
+      -DPython3_NumPy_INCLUDE_DIR=$TARGET_NUMPY_INCLUDE_DIRS \
+      -DBUILD_WITH_OPENMP_SUPPORT=ON \
+      -DBUILD_WITH_CASADI_SUPPORT=ON \
+      -DBUILD_TESTING=OFF \
+      -DPYTHON_EXECUTABLE=$PYTHON
+
+make -j16
+# make -j${CPU_COUNT}
+make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake
+  rm $PREFIX/lib/cmake/pinocchio/pinocchioTargets.cmake.back
+fi
---
+++ .github/workflows/conda/recipes/proxddp/build_proxddp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+rm -rf build
+
+mkdir build
+cd build
+
+cmake ${CMAKE_ARGS} .. \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DBUILD_PYTHON_INTERFACE=ON \
+      -DBUILD_CROCODDYL_COMPAT=ON \
+      -DBUILD_BENCHMARKS=OFF \
+      -DBUILD_EXAMPLES=OFF \
+      -DBUILD_TESTING=OFF 
+
+make -j${CPU_COUNT}
+make install
