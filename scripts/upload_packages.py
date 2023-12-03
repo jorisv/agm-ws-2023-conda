a77491f171dbca785ae7997bb81276bbddb662a0
Olivier Roussel <olivier.roussel@inria.fr>
2023-12-03 19:05:30 +0100

    Fixing GH actions.

--- scripts/upload_packages.py
+++ scripts/upload_packages.py
@@ -1,12 +1,18 @@
+from subprocess import run
 import os
 
-print('=== OUT_PACKAGES:', os.getenv('OUT_PACKAGES'))
-conda_packages = os.getenv('OUT_PACKAGES')
-if conda_packages:
-  for package in conda_packages.splitlines():
-    if '-devel' not in package and '-split' not in package:
-      os.system('anaconda -t ' + os.getenv('ANACONDA_API_TOKEN') + ' upload --user agm-ws-2023 ' + package)
-    else:
-      print('Skipping package ', package, '...')
-else:
-  print('No package to upload')
+packages = ['hpp-fcl', 'pinocchio', 'example-robot-data', 'proxnlp', 'proxddp']
+
+# collect all generated conda packages
+out_packages = []
+for package in packages:
+  out_packages.append(run("conda build --output conda/recipes/" + package, capture_output=True, shell=True, text=True).stdout.split())
+
+out_packages_flat = [item for sublist in out_packages for item in sublist]
+
+# filter runtime packages and upload them to Anaconda dedicated channel
+for package in out_packages_flat:
+  if '-devel' not in package and '-split' not in package:
+    os.system('anaconda -t ' + os.getenv('ANACONDA_API_TOKEN') + ' upload --user agm-ws-2023 ' + package)
+  else:
+    print('Skipping package ', package, '...')
